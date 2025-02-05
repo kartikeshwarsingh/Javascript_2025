4079d1f6bf96988b6f895dfa81bc0a5f6776cddf
Kartikeshwar Singh <kartikthakur465@gmail.com>
2025-02-05 07:18:00 +0000

    type_conversions

---
+++ javascript_20255/03_conversionOperations.js
@@ -0,0 +1,31 @@
+//let score = "33abc"
+//let score = null
+
+let score = true
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valInNumber = Number(score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; False => 0
+
+// let isLoggedIn = 1
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
